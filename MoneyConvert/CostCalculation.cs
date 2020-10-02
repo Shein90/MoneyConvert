39967f766a40a92b2ec3f64c5d6b958a4f1f80b6
Евгений Рахимов <Alterman69@mail.ru>
2020-10-02 19:02:54 +0700

    Public

---
+++ MoneyConvert/CostCalculation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyConvert
+{
+    //Класс принимает данные из обработчика событий, парсит информацию с сайта и возращает результат 
+    class CostCalculation
+    {
+        //сумма введеная пользователем и данные о расчитываемых валютах
+        private double sum;
+        private string firstValute;
+        private string secondValute;
+
+        public CostCalculation(double sum, string firstValute, string secondValute)
+        {
+            this.sum = sum;
+            this.firstValute = firstValute;
+            this.secondValute = secondValute;
+        }
+
+        public string Calculate()
+        {
+            string a = "Hello";
+            return a;
+        }
+
+
+    }
+}
